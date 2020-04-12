e3239c02c96b4de16fa21fe44688e588cade9228
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-04-12 16:13:43 +0800

    Add part 1 & 2 to Chapter 5.

--- 05/scene/main/Schedule.gd
+++ 05/scene/main/Schedule.gd
@@ -9,7 +9,5 @@ var _actors: Array = [null]
 func _on_InitWorld_sprite_created(new_sprite: Sprite) -> void:
 	if new_sprite.is_in_group(_new_GroupName.PC):
 		_actors[0] = new_sprite
-		print(_actors)
 	elif new_sprite.is_in_group(_new_GroupName.DWARF):
 		_actors.append(new_sprite)
-		print(_actors)
